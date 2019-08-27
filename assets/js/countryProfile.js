a956a0ad8cb6163e8b82c14e62b3f404ccbd995b
Lucy Kimotho <luciekimotho@gmail.com>
2019-08-27 09:13:40 +0300

    About (#26)
    * 1. Merged country perfomance & data visualization 2. Added links to the cards
    
    * Changed padding for header to reduce spacing on the SDG page
    
    * Removed top margin on SDG page to allow page to fit
    
    * Added enquiry email to footer
    
    * Reduced header padding
    
    * Added buttons to the sdg1 page
    
    * Remove breadcrumbs on the navigation
    
    * 1. Reduced font sizes 2. Making cards wider 3. Included links in the cards
    
    * Added disclaimer to the About us page
    
    * About us page changes: Rearranged cards
    
    * About page changes
    
    * Dashboard & about changes

--- assets/js/countryProfile.js
+++ assets/js/countryProfile.js
@@ -23,26 +23,27 @@ function loadCountryData(countryId) {
                     "totalPopulation": data[newKey].totalPopulation
                 };
                 var profile = '<div class="person-list-item">' +
-                            '<p id="countryName" style="margin-left: 10%;"><i class="fa fa-info"></i> Name: ' + countryData.name + '</p>' +
-                            '<p class="white" style="margin-left: 10%;" id="region"><i class="fa fa-globe"></i> Region: ' + countryData.region + '</p>' +
-                            '<p class="white" style="margin-left: 10%;" id="capitalCity"><i class="fa fa-map-marker"></i> Capital: ' + countryData.capital + '</p>' +
+                            '<p id="countryName" style="margin-left: 10%;"> Name: ' + countryData.name + '</p>' +
+                            '<p class="white" style="margin-left: 10%;" id="povertyLine"> Population: ' + countryData.totalPopulation +  '</p>' +
+                            '<p class="white" style="margin-left: 10%;" id="region"> Region: ' + countryData.region + '</p>' +
+                            '<p class="white" style="margin-left: 10%;" id="capitalCity"> Capital: ' + countryData.capital + '</p>' +
                             '<p class="white" style="margin-left: 10%;" id="povertyLine"> Poverty Line:  </p>' +
                             '<p class="white" style="margin-left: 10%;" id="gdpPerCapita"> GDP Per Capita:  </p>' +
                             
                 '</div>';
                 var flagURL = '<img src="'+'../' + countryData.flagURL + '" class="img-fluid" style="max-width:150px;max-height:100px;float: right;margin-right: 8%;margin-top: 1%;margin-bottom: 1%;">';
-                var countryStatistics = '<div class="col-md-6 user-pad text-left">' +
+                var countryStatistics = '<div class="col-md-6 col-sm-6 user-pad text-left">' +
                     '<h3 style="margin-left: 10%;">SIZE(sq.km)</h3>' +
                     '<h4 style="margin-left: 10%;">' + countryData.size + '</h4>' +
                     '</div>' +
-                    '<div class="col-md-6 user-pad text-center">' +
+                    '<div class="col-md-6 col-sm-6 user-pad text-center">' +
                     '<h3 style="margin-right: -54%;">POPULATION</h3>' +
                     '<h4 style="margin-right: -54%;">' + countryData.totalPopulation + '</h4>' +
                     '</div>' +
                     '</div>';
                 $('#countryProfile').empty().append(profile);
                 $('#flagURL').empty().append(flagURL);
-                $('#countryStatistics').empty().append(countryStatistics);
+               // $('#countryStatistics').empty().append(countryStatistics);
                 $('#myModal').modal('show');
             }
         }
